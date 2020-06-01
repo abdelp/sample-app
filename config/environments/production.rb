8b528f73c69166d902eda02397a8862ca6193dec
abdelp <juniorperezpy@gmail.com>
2020-06-01 23:47:10 +0000

    Update default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   host = 'https://peaceful-everglades-53157.herokuapp.com'
-
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: '587',
