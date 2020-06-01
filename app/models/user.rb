4ff3651d2bf37b125b76e9910bc2d320c24b8d46
abdelp <juniorperezpy@gmail.com>
2020-06-01 23:39:46 +0000

    Fix User.digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_secure_password
   
   def User.digest(string)
-    cost = ActivateModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                     BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
